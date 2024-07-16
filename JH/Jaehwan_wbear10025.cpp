32625500d6fd5d9cefa929c23565b4a041a1bd0b
inter16 <16interchange@gmail.com>
2024-07-17 02:03:29 +0900

    move files by Jaehwan

--- JH/Jaehwan_wbear10025.cpp
+++ JH/Jaehwan_wbear10025.cpp
@@ -1,18 +1,45 @@
 #include <iostream>
-#include <vector>0
+#include <unordered_map>
+#include <queue>
 
 using namespace std;
 
 int n, k;
+int g, x;
 
-vector<int> v(1000000);
+queue<int> q;
+unordered_map<int, int> um;
 
 int main(){
     cin >> n >> k;
     
     for (auto i = 0; i < n; i++){
-        
+        cin >> g >> x;
+        um[x] = g;
+        q.push(x);
     }
+
+    int ans = 0;
+
+    while(!q.empty()){
+        int qf = q.front();
+        int temp = 0;
+
+        for(auto i = qf - k; i <= qf + k; i++){
+            for(auto j = -k; j <= k; j++){
+                temp += um[i + j];
+            }
+        }
+
+        ans = max(temp, ans);
+    }
+
+    cout << ans;
+
+
+
+
+
 }
 /*
 
@@ -20,6 +47,6 @@ x_i에 N개 존재
 양동이 g_i개 얼음존재
 좌우 k까지 가능
 
-양동이 pairwise disjoint
+양동이 모두 위치 다름
 
 */
