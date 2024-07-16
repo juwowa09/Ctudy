3dd03de8c818889264c164b08ddf14f4275f831d
inter16 <68740146+inter16@users.noreply.github.com>
2024-07-17 04:19:28 +0900

    10025 백곰 예제 solve 메모리 초과 by Jaehwan

--- JH/240722_2_10025wbear_Jaehwan.cpp
+++ JH/240722_2_10025wbear_Jaehwan.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <unordered_map>
 #include <queue>
+#include <unordered_map>
 
 using namespace std;
 
@@ -8,39 +8,37 @@ int n, k;
 int g, x;
 
 queue<int> q;
-unordered_map<int, int> um;
+unordered_map<int, int> um; // 얼음에 비해 배열 크기 매우 크다 -> 해시?
 
-int main(){
+int main() {
 
-    cin >> n >> k;
-    
-    for (auto i = 0; i < n; i++){
-        cin >> g >> x;
-        um[x] = g;
-        q.push(x);
+  cin >> n >> k;
+
+  for (auto i = 0; i < n; i++) {
+    cin >> g >> x;
+    um[x] = g; // 얼음 양동이 있는 곳 g개, 없는 곳 0개
+    q.push(x); // 얼음 양동이 위치 입력
+  }
+
+  int ans = 0;
+
+  while (!q.empty()) {
+    int qf = q.front(); // 얼음 양동이 위치 qf
+
+    for (auto i = qf - k; i <= qf + k; i++) { // 얼음 양동이 주변만 체크
+
+      int temp = 0;
+      for (auto j = -k; j <= k; j++) {
+        temp += um[i + j]; // 얼음양동이 기준 -k부터 k까지 더하기
+      }
+
+      ans = max(temp, ans);
     }
 
-    int ans = 0;
+    q.pop(); // qf 기준 계산 끝나면 제거
+  }
 
-    while(!q.empty()){
-        int qf = q.front();
-        int temp = 0;
-
-        for(auto i = qf - k; i <= qf + k; i++){
-            for(auto j = -k; j <= k; j++){
-                temp += um[i + j];
-            }
-        }
-
-        ans = max(temp, ans);
-    }
-
-    cout << ans;
-
-
-
-
-
+  cout << ans;
 }
 /*
 
