62ce5c15129087ec9378720d7713a5ea7c8cdf16
Seonghyun Kim <archun39@kookmin.ac.kr>
2024-07-29 16:53:58 +0900

    fix 4358.cpp and finish

--- seonghyun/week2/4358.cpp
+++ seonghyun/week2/4358.cpp
@@ -30,8 +30,7 @@ int main(){
     ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
     string x;
     int countTotal = 0;
-    while(true){
-        getline(cin, x);
+    while(getline(cin,x)){
         if(x=="end") break;
         countTotal++;
         insert(x); 
