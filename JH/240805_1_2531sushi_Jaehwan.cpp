56ab638a6f5c2eac7e1f4ad9c3e48e54a6fa9917
DESKTOP-E6S0LE0\SKT012 <16interchange@gmail.com>
2024-08-04 00:57:17 +0900

    retire 2531 sushi

--- JH/240805_1_2531sushi_Jaehwan.cpp
+++ JH/240805_1_2531sushi_Jaehwan.cpp
@@ -1,81 +1,50 @@
 #include <iostream>
-#include <queue>
-#include <set>
+#include <vector>
+#include <map>
 
 using namespace std;
 
-
-int N, d, k, c;
-
-queue<int> q;
-int ans;
-
 int main(){
+
+    int N, d, k, c;
+    int ans = 0;
+
     cin >> N >> d >> k >> c;
+
+    vector<int> arr(N);
 
     for(auto i = 0; i < N; i++){
         int temp;
         cin >> temp;
-        q.push(temp);
+        arr[i] = temp;
     }
 
-    int kMax = 0;
-    bool get_c = false;
-    bool check_k = true;
-    for(auto i = 0; i < N; i++){
-        queue<int> tempq = q;
-        set<int> temps;
-        bool is_c = true;
-    
-        if(check_k){
-            for(auto j = 0; j < k; j++){
-                
-                int qfront = tempq.front();
-                temps.insert(qfront);
-                tempq.pop();
-                tempq.push(qfront);
-            }
-        }
+    map<int, int> m;
+    bool getc = false;
 
-        tempq = q;
-
-        if(!get_c && q.front() == c){
-            bool is_c = false;
-            int qfront = tempq.front();
-            tempq.pop();
-            tempq.push(c);
-
-            for(auto j = 1; j < k; j++){
-                
-                int qfront = tempq.front();
-                if (qfront == c) is_c = true; break;
-                tempq.pop();
-                tempq.push(qfront);
-            }
-
-        }
-
-
-
-
-
-        if(check_k){
-            kMax = max(kMax, int(temps.size()));
-        }
-
-        
-        if (kMax == k) check_k = false;
-        if (is_c == false) get_c = true;
-
-        if(!check_k && get_c) break;
-
-        int qfront = q.front();
-        q.pop();
-        q.push(qfront);
+    for(auto i = 0; i < k; i++){
+        m[arr[i]]++;
     }
 
-    if (get_c == true) ans = kMax + 1;
-    else ans = kMax;
+
+    int kMax = m.size();
+    if(m.find(c) == m.end()) getc = true;
+
+    if(N > k){
+        for(auto i = 1; i <= N; i++){
+            m[arr[i - 1]]--;
+            if (m[arr[i - 1]] == 0) m.erase(arr[i - 1]);
+
+            if (i + k - 1 >= N) m[arr[i + k - 1 - N]]++;
+            else m[arr[i + k - 1]]++;
+
+            kMax = max(kMax, int(m.size()));
+            if(m.find(c) == m.end()) getc = true;    
+        }
+    }
+
+    if(!getc) ans = kMax;
+    else ans = kMax + 1;
 
     cout << ans;
 
