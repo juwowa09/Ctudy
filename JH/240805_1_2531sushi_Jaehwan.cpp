f2feb34f0de08051753fb272c46a32cda275b020
DESKTOP-E6S0LE0\SKT012 <16interchange@gmail.com>
2024-08-03 21:21:37 +0900

    2531 sushi 2nd Timeout

--- JH/240805_1_2531sushi_Jaehwan.cpp
+++ JH/240805_1_2531sushi_Jaehwan.cpp
@@ -1,100 +1,82 @@
 #include <iostream>
+#include <queue>
 #include <set>
 
 using namespace std;
 
+
 int N, d, k, c;
-int arr[30000]; // vector 이용해서 find?
+
+queue<int> q;
 int ans;
-bool noSushi;
-
-
 
 int main(){
     cin >> N >> d >> k >> c;
 
-
     for(auto i = 0; i < N; i++){
-        cin >> arr[i];
+        int temp;
+        cin >> temp;
+        q.push(temp);
     }
 
+    int kMax = 0;
+    bool get_c = false;
+    bool check_k = true;
     for(auto i = 0; i < N; i++){
-        if (arr[i] == c){
-            if(i <= N - k){
-
-                set<int> s;
-                s.insert(c);
-                for (auto j = 1; j < k; j++){
-                    s.insert(arr[i+j]);
-                    if(arr[i+j] == c) noSushi = true;
-                }.
-
-                int sSize = s.size();
-                
-                ans = max(ans, sSize);
-            }
-
-            else{
-
-                set<int> s;
-                s.insert(c);
-                for (auto j = i + 1; j < N; j++){
-                    s.insert(arr[j]);
-                    if(arr[j] == c) noSushi = true;
-                }
-                for (auto j = 0; j < i - N + k; j++){
-                    if(arr[j] == c) noSushi = true;
-                    s.insert(arr[j]);
-                }
-
-                int sSize = s.size();
-
-                ans = max(ans, sSize);
-            }
-        }
-
-        else{
-            if(i <= N - k){
-
-                set<int> s;
-                for (auto j = 0; j < k; j++){
-                    s.insert(arr[i+j]);
-                }
-
-                int sSize = s.size();
-
-                ans = max(ans, sSize);
-            }
-
-            else{
-
-                set<int> s;
-                for (auto j = i; j < N; j++){
-                    s.insert(arr[j]);
-                }
-                for (auto j = 0; j < i - N + k; j++){
-                    s.insert(arr[j]);
-
-                }
-
-                int sSize = s.size();
-
-                ans = max(ans, sSize);
-            }
-        }
-    }
-
-    if (noSushi){
-        cout << ans;
-    }
-
-    else{
-        cout << ans + 1;
-    }
-
+        queue<int> tempq = q;
+        set<int> temps;
+        bool is_c = true;
     
-}
+        if(check_k){
+            for(auto j = 0; j < k; j++){
+                
+                int qfront = tempq.front();
+                temps.insert(qfront);
+                tempq.pop();
+                tempq.push(qfront);
+            }
+        }
 
-/*
-k <= n, c <= d
-*/
+        tempq = q;
+
+        if(!get_c && q.front() == c){
+            bool is_c = false;
+            int qfront = tempq.front();
+            tempq.pop();
+            tempq.push(c);
+
+            for(auto j = 1; j < k; j++){
+                
+                int qfront = tempq.front();
+                if (qfront == c) is_c = true; break;
+                tempq.pop();
+                tempq.push(qfront);
+            }
+
+        }
+
+
+
+
+
+        if(check_k){
+            kMax = max(kMax, int(temps.size()));
+        }
+
+        
+        if (kMax == k) check_k = false;
+        if (is_c == false) get_c = true;
+
+        if(!check_k && get_c) break;
+
+        int qfront = q.front();
+        q.pop();
+        q.push(qfront);
+    }
+
+    if (get_c == true) ans = kMax + 1;
+    else ans = kMax;
+
+    cout << ans;
+
+}
