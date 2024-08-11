46757f8a766f5141034b1e736c9328bafb9b43f0
inter16 <16interchange@gmail.com>
2024-08-11 23:34:12 +0900

    2075 Timeout

--- JH/240812_2_2075Nthn_Jaehwan.cpp
+++ JH/240812_2_2075Nthn_Jaehwan.cpp
@@ -1,44 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-int N;
-
-int ans;
-
 int main(){
+    int N;
+
     cin >> N;
 
-    vector<vector<int>> arr(N, vector<int>(N));
-    vector<int> vinit(N, N-1);
-    vector<int> vMax(N);
+    priority_queue<pair<int, int>> pq;
+    vector<int> ind(N, N-1);
 
+    vector<vector<int>> arr(N, vector<int>(N));
+
+    int ans;
 
     for(auto i = 0; i < N; i++){
-        for (auto j = 0; j < N; j++){
+        for(auto j = 0; j < N; j++){
             cin >> arr[i][j];
+
+            if(i == N - 1){
+                pq.push({arr[i][j], j});
+            }
         }
     }
 
 
-    for (int i = 0; i < N; i++) {
-        vMax[i] = arr[N-1][i];
-    }
-
-
-    for (auto k = 0; k < N; k++){
-        auto temp_it = max_element(vMax.begin(), vMax.end());
-        int ind = temp_it - vMax.begin();
-        ans = *temp_it;
-
-
-        vinit[ind]--;
-        if (vinit[ind] >= 0) {
-            vMax[ind] = arr[vinit[ind]][ind];
-        } else {
-            vMax[ind] = -2e9;
+    for(auto i = 0; i < N; i++){
+        ans = pq.top().first;
+        int tempx = pq.top().second;
+        pq.pop();
+        
+        ind[tempx]--;
+        
+        if (ind[tempx] >= 0) {
+            pq.push({arr[ind[tempx]][tempx], tempx});
         }
     }
 
