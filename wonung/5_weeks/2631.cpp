d649c41be421777ee5663dfc254e686f9cef14c0
juwowa09 <juwowa09@gmail.com>
2024-09-04 16:39:27 +0900

    고층건물: 1027

--- wonung/5_weeks/2631.cpp
+++ wonung/5_weeks/2631.cpp
@@ -7,4 +7,24 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
+    int n, result = 0;
+    cin >> n;
+    int *arr = new int[n];
+    int *dp = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        dp[i] = 1;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[i] > arr[j] && dp[j] >= dp[i])
+            {
+                dp[i] = dp[j] + 1;
+            }
+        }
+        if (dp[i] > result)
+            result = dp[i];
+    }
+    cout << n - result;
 }
