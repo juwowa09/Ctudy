c1dd8b4d23a53920661d1bd632d53cd823741f5a
juwowa09 <juwowa09@gmail.com>
2024-09-28 16:50:41 +0900

    트리의 지름: 1167, DFS verstion

--- wonung/7_weeks/1167.cpp
+++ wonung/7_weeks/1167.cpp
@@ -3,29 +3,13 @@
 #include <queue>
 using namespace std;
 
-void dijkstra(vector<pair<int, int>> *v, int st, int *d, int n)
+void dfs(vector<pair<int, int>> *v, pair<int, int> *p, int *visit, int prev)
 {
-    fill(d, d + n + 1, (int)1e9);
-    d[st] = 0;
-    priority_queue<pair<int, int>,
-                   vector<pair<int, int>>,
-                   greater<pair<int, int>>>
-        heap;
-    heap.push({d[st], st});
-    while (!heap.empty())
-    {
-        auto cur = heap.top();
-        heap.pop();
-        if (cur.first != d[cur.second])
-            continue;
-        for (auto it : v[cur.second])
-        {
-            if (d[it.first] < cur.first + it.second)
-                continue;
-            d[it.first] = cur.first + it.second;
-            heap.push({d[it.first], it.first});
-        }
-    }
+    if (visit[p->first] != -1)
+        return;
+    visit[p->first] = prev + p->second;
+    for (auto it : v[p->first])
+        dfs(v, &it, visit, visit[p->first]);
 }
 
 int main()
@@ -36,11 +20,12 @@ int main()
     int n, st, en, w, max = 0, idx;
 
     cin >> n;
-    int *arr = new int[n + 1];
+    int *visit = new int[n + 1];
     vector<pair<int, int>> *v = new vector<pair<int, int>>[n + 1];
 
     for (int i = 0; i < n; i++)
     {
+        visit[i] = -1;
         cin >> st;
         cin >> en;
         while (en != -1)
@@ -50,21 +35,27 @@ int main()
             cin >> en;
         }
     }
-    dijkstra(v, 1, arr, n);
-    for (int i = 1; i < n + 1; i++)
+    visit[n] = -1;
+    pair<int, int> p = {1, 0};
+    dfs(v, &p, visit, 0);
+    for (int i = 0; i < n + 1; i++)
     {
-        if (arr[i] > max)
+        if (max < visit[i])
         {
-            max = arr[i];
+            max = visit[i];
             idx = i;
         }
+        visit[i] = -1;
     }
     max = 0;
-    dijkstra(v, idx, arr, n);
-    for (int i = 1; i < n + 1; i++)
+    p = {idx, 0};
+    dfs(v, &p, visit, 0);
+    for (int i = 0; i < n + 1; i++)
     {
-        if (arr[i] > max)
-            max = arr[i];
+        if (max < visit[i])
+            max = visit[i];
     }
     cout << max;
+    delete[] visit;
+    delete[] v;
 }
