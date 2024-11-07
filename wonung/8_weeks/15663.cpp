0b7d00148a30de293e9b17ccd1b16a91811c74fb
juwowa09 <juwowa09@gmail.com>
2024-11-07 15:14:17 +0900

    N과 M: 15663, dfs, 순열, 중복방지

--- wonung/8_weeks/15663.cpp
+++ wonung/8_weeks/15663.cpp
@@ -6,8 +6,10 @@ int arr[8];
 int visit[8];
 int result[8];
 
-void dfs(int d, int m, int n)
+void dfs(int tmp, int d, int m, int n, int idx)
 {
+    if (idx == n)
+        return;
     if (d == m)
     {
         for (int i = 0; i < m; i++)
@@ -17,18 +19,15 @@ void dfs(int d, int m, int n)
         cout << "\n";
         return;
     }
-    int tmp = 0;
-    for (int i = 0; i < n; i++)
+    if (visit[idx] == 0 && tmp != arr[idx])
     {
-        if (visit[i] == 0 && tmp != arr[i])
-        {
-            visit[i] = 1;
-            tmp = arr[i];
-            result[d] = arr[i];
-            dfs(d + 1, m, n);
-            visit[i] = 0;
-        }
+        visit[idx] = 1;
+        result[d] = arr[idx];
+        tmp = arr[idx];         // tmp = 중복방지 수
+        dfs(0, d + 1, m, n, 0); // 조건만족, 다음 수 찾기
+        visit[idx] = 0;
     }
+    dfs(tmp, d, m, n, idx + 1);
 }
 
 int main()
@@ -45,6 +44,7 @@ int main()
         visit[i] = 0;
         result[i] = 0;
     }
+
     sort(arr, arr + n);
-    dfs(0, m, n);
+    dfs(0, 0, m, n, 0);
 }
