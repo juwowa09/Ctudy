6069b28dc8c46ee2a7fa252ff6e4dc4eb85bea2e
juwowa09 <juwowa09@gmail.com>
2024-12-31 02:33:03 +0900

    이친수: 2193, 2D DP

---
+++ wonung/11_weeks/2193(2D).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+long dp[91][2];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    dp[1][0] = 1;
+    dp[1][1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+        dp[i][1] = dp[i - 1][0];
+    }
+    cout << dp[n][1];
+}
