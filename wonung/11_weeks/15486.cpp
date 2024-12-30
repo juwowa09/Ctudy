96e7def5c35aa4c3152b5e5a6497d1b7c9849a99
juwowa09 <juwowa09@gmail.com>
2024-12-31 00:26:10 +0900

    퇴사 2: 15486, DP

---
+++ wonung/11_weeks/15486.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int dp[1500051] = {
+    0,
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n, t, p;
+
+    cin >> n;
+    for (int i = 1; i < n + 1; i++)
+    {
+        cin >> t >> p;
+        dp[i] = dp[i] < dp[i - 1] ? dp[i - 1] : dp[i];
+        dp[i + t] = dp[i + t] < dp[i] + p ? dp[i] + p : dp[i + t];
+    }
+    dp[n + 1] = dp[n + 1] < dp[n] ? dp[n] : dp[n + 1];
+    cout << dp[n + 1];
+}
