b5f47532cfce759c809fa61ac369833802354bb8
juwowa09 <juwowa09@gmail.com>
2025-01-01 20:45:08 +0900

    오르막 수: 11057, DP 1d

--- wonung/12_weeks/11057.cpp
+++ wonung/12_weeks/11057.cpp
@@ -8,18 +8,16 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n, result = 0;
+    int n;
     vector<int> dp(10, 1);
     vector<int> prev(10, 1);
     cin >> n;
-    while (--n)
+    while (n--)
     {
         dp[9] = prev[9];
         for (int j = 8; j >= 0; j--)
             dp[j] = (prev[j] + dp[j + 1]) % 10007;
         prev = dp;
     }
-    for (int i = 0; i <= 9; i++)
-        result = (result + dp[i]) % 10007;
-    cout << result;
+    cout << dp[0];
 }
