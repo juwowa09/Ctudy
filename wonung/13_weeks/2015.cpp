012f64bcb77243c86e7478f41a27a84a859d9a20
juwowa09 <juwowa09@gmail.com>
2025-01-08 16:39:30 +0900

    수들의 합4: 2015, 누적합, map 사용

--- wonung/13_weeks/2015.cpp
+++ wonung/13_weeks/2015.cpp
@@ -23,7 +23,7 @@ int main()
             result += m[sum - k]; // 누적합이므로 map 에는 현재 앞에 존재하는 누적합에 대한 정보만 들어있음.
         }
         m[sum]++;
-        //        result += m[v[i]-k]; k 가 0 인 경우 m[v[i]]++ 먼저하면 안됨.
+        //       result += m[v[i]-k]; k 가 0 인 경우 m[v[i]]++ 먼저하면 안됨.
     }
     cout << result;
 }
